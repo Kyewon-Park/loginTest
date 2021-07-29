9012c62041194adf146a4cc69eee2b5a4473aa80
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-07-29 12:24:24 +0900

    crossOrigin추가

--- aspS/src/main/java/as/aspS/AspSApplication.java
+++ aspS/src/main/java/as/aspS/AspSApplication.java
@@ -1,13 +1,18 @@
 package as.aspS;
 
+import as.aspS.domain.Member;
+import as.aspS.repository.MemberJPARepository;
+import as.aspS.service.MemberJPAService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class AspSApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AspSApplication.class, args);
 	}
-
 }
