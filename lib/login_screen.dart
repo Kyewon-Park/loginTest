7fa836dcd69fcb081c6876448aa583b88af5e1ca
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-07-30 12:55:40 +0900

    http post method login tested

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,15 +1,95 @@
+import 'dart:convert';
+import 'package:androidstudioprojects/buttons/widePostButton.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final idController = TextEditingController();
+  final pwController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('First Route'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Spacer(),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: idController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid mail id as abc@gmail.com'
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                controller: pwController,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter your secure password'
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: (){
+                //TODO FORGOT PASSWORD SCREEN GOES HERE
+              },
+              child: Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+            Spacer(),
+            WidePostButton(buttonName: "Login", callback: login,),
+            Spacer(),
+            WidePostButton(buttonName: "Register", callback: register,),
+            Spacer(),
+          ],
+        ),
       ),
-      body: Container(
-        child: Text("login screen"),
-      )
     );
   }
+
+  Future<http.Response> login() {
+    return http.post(
+      Uri.parse('http://192.168.0.8:8080/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': idController.text,
+        'password': pwController.text
+      }),
+    );
+  }
+
+  Future<http.Response> register() {
+    return http.post(
+      Uri.parse('http://192.168.0.8:8080/register'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'email': idController.text,
+        'password': pwController.text
+      }),
+    );
+  }
+
 }
+
