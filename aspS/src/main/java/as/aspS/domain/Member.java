992d9fce24d2a1116497d43754fcc7eb768f3717
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-08-06 19:19:20 +0900

    Member field 추가

--- aspS/src/main/java/as/aspS/domain/Member.java
+++ aspS/src/main/java/as/aspS/domain/Member.java
@@ -2,26 +2,34 @@ package as.aspS.domain;
 
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.Date;
 
-@Entity
+@Entity //JPA가 관리하는 객체이며, db테이블과 매핑하여 쓸 것임.
 @Getter
 public class Member {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "member_id")
     private Long id;
+    @Column(unique = true, nullable = false)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
+    private String name;
+    //@Temporal 생략 가능
+    private LocalDate localDate;
 
     public Member() {
     }
 
-    public Member(String email, String password) {
+    public Member(String email, String password, String name) {
         this.email = email;
         this.password = password;
+        this.name = name;
+        this.localDate = LocalDate.now();
     }
 }
