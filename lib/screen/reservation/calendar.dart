0cc63a8b84df3705a0a7f2ff313d7a7bcd9d5951
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-08-10 16:44:16 +0900

    added flexible ListWheelScrollView

--- lib/screen/reservation/calendar.dart
+++ lib/screen/reservation/calendar.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -11,22 +9,14 @@ class Calendar extends StatefulWidget {
 }
 
 class _CalendarState extends State<Calendar> {
-  late final ValueNotifier<List<Event>> _selectedEvents;
   CalendarFormat format = CalendarFormat.month;
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
-  }
-
-  List<Event> _getEventsForDay(DateTime day) {
-    return events[day] ?? [];
-  }
+  int selected = 5;
+  int itemCount = 12;
+  FixedExtentScrollController _scrollController =
+      FixedExtentScrollController(initialItem: 5);
 
   @override
   void dispose() {
@@ -37,51 +27,86 @@ class _CalendarState extends State<Calendar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TableCalendar(
-        headerStyle: HeaderStyle(
-          formatButtonVisible: false,
-          titleCentered: true,
-        ),
-        firstDay: DateTime.utc(2010, 10, 16),
-        lastDay: DateTime.utc(2030, 3, 14),
-        focusedDay: _focusedDay,
-
-        //이벤트
-        eventLoader: (day){
-          return _getEventsForDay(day);
-        },
-
-        //누른 날짜 마킹
-        selectedDayPredicate: (DateTime date){
-          return isSameDay(_selectedDay, date);
-        },
-        onDaySelected: (selectedDay, focusedDay) {
-          setState(() {
-            _selectedDay = selectedDay;
-            _focusedDay = focusedDay; // update `_focusedDay` here as well
-          });
-        },
-        onPageChanged: (focusedDay){
-          _focusedDay = focusedDay;
-        },
-
-        //달력 스타일 조정
-        calendarStyle: CalendarStyle(
-          selectedDecoration: BoxDecoration(
-            color: Colors.orangeAccent,
+        body: Column(
+      children: [
+        TableCalendar(
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
           ),
-          todayDecoration: BoxDecoration(
-            color: Colors.blue,
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: _focusedDay,
+          calendarBuilders: CalendarBuilders(
+            selectedBuilder: (context, date, events) => Container(
+                margin: const EdgeInsets.all(4.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Text(
+                  date.day.toString(),
+                  style: TextStyle(color: Colors.white),
+                )),
+            todayBuilder: (context, date, events) => Container(
+                margin: const EdgeInsets.all(4.0),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.orange,
+                    borderRadius: BorderRadius.circular(10.0)),
+                child: Text(
+                  date.day.toString(),
+                  style: TextStyle(color: Colors.white),
+                )),
+          ),
 
-          )
+          //누른 날짜 마킹
+          selectedDayPredicate: (DateTime date) {
+            return isSameDay(_selectedDay, date);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay; // update `_focusedDay` here as well
+            });
+          },
+          onPageChanged: (focusedDay) {
+            _focusedDay = focusedDay;
+          },
         ),
-
-        //
-
-
-      ),
-    );
+        Flexible(
+          child: RotatedBox(
+            quarterTurns: 3,
+            child: new ListWheelScrollView(
+              itemExtent: 80,
+              diameterRatio: 1.7,
+              onSelectedItemChanged: (x) {
+                setState(() {
+                  selected = x;
+                });
+              },
+              physics: FixedExtentScrollPhysics(),
+              children: List<Widget>.generate(
+                  13, (index) => AnimatedContainer(
+                        duration: Duration(milliseconds: 300),
+                        width: index == selected ? 60 : 50,
+                        height: index == selected ? 60 : 50,
+                        decoration: BoxDecoration(
+                            color: index == selected ? Colors.blue : Colors.blue[300],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: RotatedBox(
+                          quarterTurns: 1,
+                          child: Center(
+                              child: Text(
+                            '${index + 10} 시',
+                          )),
+                        ),
+                      )),
+            ),
+          ),
+        ),
+      ],
+    ));
   }
-
-
 }
