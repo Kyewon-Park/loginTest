2d3a83347a39b6bc77f216596a873b1932883328
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-08-24 14:40:07 +0900

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:androidstudioprojects/provider/member_provider.dart';
 import 'package:androidstudioprojects/screen/welcome_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MemberProvider()),
+      ],
+      child: MyApp(),)
+  );
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/provider/member_provider.dart
@@ -0,0 +1,6 @@
+import 'package:androidstudioprojects/model/Member.dart';
+import 'package:flutter/cupertino.dart';
+
+class MemberProvider extends ChangeNotifier{
+  late Member member;
+}
