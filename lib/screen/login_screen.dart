5a5810b45e2667cd5aec7a084dd3e4ecb3ab9d4d
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-08-26 13:35:52 +0900

    Update login_screen.dart

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:androidstudioprojects/buttons/widePostButton.dart';
 import 'package:androidstudioprojects/model/Member.dart';
+import 'package:androidstudioprojects/screen/register_screen.dart';
 import 'package:androidstudioprojects/screen/reservation/show_month.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -17,70 +18,6 @@ class _LoginScreenState extends State<LoginScreen> {
   final idController = TextEditingController();
   final pwController = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false, //키보드가 화면 줄이는 현상 방지
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Spacer(flex: 2,),
-            Container(
-                color: Colors.white,
-                height: 20.h,
-                child: Image.asset('assets/bunny.png', fit: BoxFit.fitHeight,)
-            ),
-            Spacer(flex: 2,),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child: Container(
-                width: 80.w,
-                child: TextField(
-                  controller: idController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                      hintText: 'Enter valid mail id as abc@gmail.com'
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child: Container(
-                width: 80.w,
-                child: TextField(
-                  controller: pwController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter your secure password'
-                  ),
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Spacer(flex: 2,),
-            WidePostButton(buttonName: "Login", callback: login),
-            Spacer(),
-            WidePostButton(buttonName: "Register", callback: register,),
-            Spacer(flex: 3,),
-          ],
-        ),
-      ),
-    );
-  }
-
   Future<Member?> login() async{
     await http.post(
       Uri.parse('http://192.168.137.1:8080/login'), //192.168.0.8:8080
@@ -92,33 +29,93 @@ class _LoginScreenState extends State<LoginScreen> {
         'password': pwController.text
       }),
     ).then((value){
-        print(value.body);
-        print(value.statusCode);
-        if (value.statusCode == 200) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => ShowMonth()),
-          );
-          Member member = Member.fromJson(jsonDecode(value.body));
-          print("${member.memberId}, ${member.email}, ${member.name}");
-          return member;
-        } else {
-          throw Exception('Failed to create member.');
-        }});
-        return null;
-    }
-
-  Future<http.Response> register() {
-    return http.post(
-      Uri.parse('http://192.168.0.8:8080/register'),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(<String, String>{
-        'email': idController.text,
-        'password': pwController.text
-      }),
-    );
+      print(value.body);
+      print(value.statusCode);
+      if (value.statusCode == 200) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ShowMonth()),
+        );
+        Member member = Member.fromJson(jsonDecode(value.body));
+        print("${member.memberId}, ${member.email}, ${member.name}");
+        return member;
+      } else {
+        throw Exception('Failed to create member.');
+      }});
+    return null;
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false, //키보드가 화면 줄이는 현상 방지
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Spacer(flex: 2,),
+              Container(
+                  color: Colors.white,
+                  height: 20.h,
+                  child: Image.asset('assets/bunny.png', fit: BoxFit.fitHeight,)
+              ),
+              Spacer(flex: 2,),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Container(
+                  width: 80.w,
+                  child: TextField(
+                    controller: idController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter valid mail id as abc@gmail.com'
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Container(
+                  width: 80.w,
+                  child: TextField(
+                    controller: pwController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter your secure password'
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: (){
+                  //TODO FORGOT PASSWORD SCREEN GOES HERE
+                },
+                child: Text(
+                  'Forgot Password',
+                  style: TextStyle(color: Colors.blue, fontSize: 11.sp),
+                ),
+              ),
+              Spacer(flex: 2,),
+              WidePostButton(buttonName: "Login", callback: login),
+              Spacer(),
+              WidePostButton(buttonName: "Register", callback: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SafeArea(child: RegisterScreen())),
+                );
+              }),
+              Spacer(flex: 3,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
