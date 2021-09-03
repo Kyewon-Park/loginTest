bb854f3af4c0c25bede9b5ad73b909a9e5791dab
박계원 <62559049+Kyewon-Park@users.noreply.github.com>
2021-09-03 13:25:35 +0900

    Update mypage.dart

--- lib/screen/individual/mypage.dart
+++ lib/screen/individual/mypage.dart
@@ -1,5 +1,7 @@
 import 'package:androidstudioprojects/model/Member.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class MyPage extends StatelessWidget {
   final Member member;
@@ -7,8 +9,137 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Center(child: Text('member.name = ${member.name}'),)
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0, 5.h, 0, 0),
+      child: Column(
+        children: <Widget>[
+          Center(child:CircleAvatar(
+            backgroundColor: Colors.grey,
+            radius: 50,
+          )),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Text(member.name,
+              style: TextStyle(
+                color: Colors.teal,
+                fontSize: 20.sp,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 35,
+            width: 250,
+            child: Divider(
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10,),
+            child: Container(
+              width: 85.w,
+              height: 5.h,
+              color: Colors.teal[50],
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                  Text("-q394304",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.sp
+                    ),
+                  ),
+                ],
+
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10,),
+            child: Container(
+              width: 85.w,
+              height: 5.h,
+              color: Colors.teal[50],
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                  Text("-q394304",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.sp
+                    ),
+                  ),
+                ],
+
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10,),
+            child: Container(
+              width: 85.w,
+              height: 5.h,
+              color: Colors.teal[50],
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                  Text("-q394304",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.sp
+                    ),
+                  ),
+                ],
+
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10,),
+            child: Container(
+              width: 85.w,
+              height: 5.h,
+              color: Colors.teal[50],
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                  Text("-q394304",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.sp
+                    ),
+                  ),
+                ],
+
+              ),
+            ),
+          ),
+        ],),
     );
   }
+
 }
